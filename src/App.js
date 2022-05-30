fa4f2dc62da01972af3f93dc9391cca0461d9acf
Allan Gadelha <allangadelha@gmail.com>
2022-05-30 20:11:56 -0300

    Inserindo CSS: global e por componente

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Frase from './components/Frase';
 import HelloWorld from './components/HelloWorld';
 import People from './components/People';
 import SayMyName from './components/SayMyName';
@@ -7,12 +8,14 @@ function App() {
   
   return (
     <div className="App">
+      <h1>Treinando CSS</h1>
       <HelloWorld />
+      <Frase />
       <SayMyName name="Allan"/>
       <People 
         nome="Allan Gadelha"
         foto="https://miro.medium.com/fit/c/176/176/0*v06bUnSTYM39tuoR"
-        idade="411"
+        idade="41"
         profissao="Dev full-stack"
       />
     </div>
--- src/components/Frase.js
+++ src/components/Frase.js
@@ -1,8 +1,11 @@
+import stylesFrases from './Frase.module.css';
+
 function Frase() {
 
     return (
-
-        <p>Componente criado para testar reutilização de componente</p>
+        <div className={stylesFrases.frasecontainer}>
+            <p className={stylesFrases.frasecontent}>Componente criado para testar reutilização de componente</p>
+        </div>
 
     )
 }
