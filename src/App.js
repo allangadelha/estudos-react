dbd9d9a7cfcd5b95d64152eee61453c110d2d010
Allan Gadelha <allangadelha@gmail.com>
2022-05-30 19:03:12 -0300

    Iniciando com manipulação de JSX

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
 
 function App() {
+
+  const name = 'allan';
+  const newName = name.toUpperCase(name);
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = 'https://via.placeholder.com/150';
+
   return (
     <div className="App">
-      <h1>Olá React!</h1>
-      <p>Meun primeiro app</p>
+      <h1>Modificando o JSX!</h1>
+      <p>Olá, { newName }</p>
+      <p>Soma de 2 + 2 = { sum(2,2) }</p>
+      <img src={ url } alt="Imagem de teste"/>
     </div>
   );
 }
