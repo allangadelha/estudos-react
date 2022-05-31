fb4edab38d14a53863db2c47d17da6c6921e80d1
Allan Gadelha <allangadelha@gmail.com>
2022-05-31 10:53:25 -0300

    Hook useState

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,18 +1,43 @@
+import { useState } from 'react';
 function Form() {
 
 
     function handleCreate(e) {
         e.preventDefault();
-        console.log("Cadastrado com sucesso!!!");
+        console.log(`Cadastrado com sucesso!!!\nNome: ${name}\nSenha: ${password}`);
     }
+
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
 
     return(
         <div>
             <h1>Formuláriod e cadastro:</h1>
             <form onSubmit={handleCreate}>
                 <div>
-                    <input type="text" placeholder="Digite seu nome"/>
-                    <input type="submit" value="Cadastrar" />
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="submit" 
+                        value="Cadastrar" />
                 </div>
             </form>
         </div>
